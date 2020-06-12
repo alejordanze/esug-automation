127584a8f6633c5ecd3b623592f4dbda7510b7c9
IvySaskia <saskiaivy@outlook.com>
2020-06-12 18:56:10 -0400

    Added features/step_definitions/attendee_steps.rb

---
+++ features/step_definitions/attendee_steps.rb
@@ -0,0 +1,66 @@
+Given('I am on the ESUG homepage') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given('I click the {string} link') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I enter the required fields for user as show below') do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click the {string} button') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I enter the required fields for contact as show below') do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I select option {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I enter the required fields for conference as show below') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I select {string} option {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I select checkbox {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I enter the required fields for billing as show below') do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I see addition section') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I see payment section') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('the Registration Summary is show') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('my {string} name is {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I click {string} button') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I download the invoice') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
