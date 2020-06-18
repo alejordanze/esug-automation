e5692b7d61fdf4ce6ea21ec07ef82602752f2499
alejordanze <alejordanze@gmail.com>
2020-06-18 15:21:29 -0400

    working scenarios and user deleting hook

--- features/group_manager.feature
+++ features/group_manager.feature
@@ -3,10 +3,66 @@ Feature: ESUG register as group manager
    I want to test ESUG registration page for group managers
    so I can add group members for talks
 
+Background: I'm on the login page
+    Given I am on the login page
 
-Scenario: Leave fields in blank in group manager register page      
-	Given I am on the login page
+Scenario: Leave fields in blank in group manager register page          
     And I press "Group Manager" button
 	And I left all fields in blank
 	When I press the "Next" button
-	Then error "This field is required." is showed
+	Then error message "This field is required." is showed
+
+Scenario: Verify new user registration
+    And I press "Group Manager" button
+    And I fill the required fields as below
+    |First Name: 	    | Miguel         |
+    |Last Name: 	    | Jordan         |
+    |Email: 	        | mail@mail.com  |
+    |Password:          | 12345678       |
+    |Confirm Password:  | 12345678       |
+    And I press the Next button in group manager form
+    And I fill the required fields as below
+    |Address: 	    | Av. America         |
+    |City: 	        | Cochabamba  |
+    |Zip Code:      | 1234       |
+    |Country: 	    | Bolivia         |
+    And I click the Next button in contact form
+    And I fill the required fields as below
+    |Name: 	    | Miguel Jordan       |
+    |Address 1: 	    | Av. America         |
+    |Country 1: 	    | Bolivia         |
+    |City 1: 	        | Cochabamba  |
+    |Zip Code 1:     | 1234       |
+    When I press Finalize Registration button on Group Manager registration
+    Then Should show "Registration Summary" page
+
+Scenario: Verify new group manager login
+    And I login with data below
+    |Email:   | mail@mail.com |
+    |Password: | 12345678      |
+    When I press Login button
+    Then Should show "ESUG 2019 Date and Location Announced" page
+
+@deleteUser
+Scenario: Verify new group user registration
+    And I login with data below
+    |Email:   | mail@mail.com |
+    |Password: | 12345678      |
+    And I press Login button
+    And I press "Group Management" button
+    And I press "Register new group user" button
+    And I fill the register as below
+    |First Name: 	    | Esteban         |
+    |Last Name: 	    | Garcia         |
+    |Email: 	        | egarcia@mail.com  |
+    |Password:          | 12345678       |
+    |Confirm Password:  | 12345678       |
+    And I press Next button on user 1
+    And I fill the register as below
+    |Gender: 	    | Male         |
+    |Tshirt Size: 	    | XL         |
+    |Food Preference: 	| Vegan  |
+    And I press Next button on conference 1
+    And I left all fields in blank
+    When I press Finalize Registration button on user 1
+    Then Should show the user "egarcia@mail.com" in the table
--- features/step_definitions/group_manager.rb
+++ features/step_definitions/group_manager.rb
@@ -11,6 +11,22 @@ Given('I press {string} button') do |buttonName|
     click_link(buttonName)
 end
 
+Given('I click the {string} buttonlink') do |buttonName|
+    find(:css, '#contactInfoForm-nextButton').click
+end
+
+Given('I press the Next button in group manager form') do
+    find(:xpath, '/html/body/div/div[1]/div/form/div/div[2]/a[2]').click
+end
+  
+Given('I click the Next button in contact form') do
+    find(:css, '#contactInfoForm-nextButton').click
+end
+  
+When('I press Finalize Registration button on Group Manager registration') do
+    find(:xpath, '/html/body/div/div[1]/div/form/div/div[4]/button').click 
+end
+
 When('I left all fields in blank') do
     log('to be implemented')
 end
@@ -19,6 +35,118 @@ When('I press the {string} button') do |buttonName|
     find(:xpath, '/html/body/div/div[1]/div/form/div/div[2]/a[2]').click
 end
   
-Then('error {string} is showed') do |error|
+Then('error message {string} is showed') do |error|
     page.has_content?(error)
+end
+
+
+Given('I fill the required fields as below') do |table|
+    data = table.rows_hash
+    data.each_pair do |key, value|
+        case key
+            when "First Name:"
+                fill_in 'firstName', :with => value
+            when "Last Name:"
+                fill_in 'lastName', :with => value
+            when "Email:"
+                fill_in 'email', :with => value
+            when "Password:"
+                fill_in 'passwordRegister', :with => value
+            when "Confirm Password:"
+                fill_in 'confirmPasswordRegister', :with => value
+            when "Address:"
+                fill_in 'address', :with => value
+            when "City:"
+                fill_in 'city', :with => value
+            when "Country:"
+                find('#contactCountry').find(:xpath, '/html/body/div/div[1]/div/form/div/div[3]/div[5]/div/select/option[29]').select_option
+            when "Zip Code:"
+                fill_in 'postal', :with => value
+            when "Name:"
+                fill_in 'organizationName', :with => value
+            when "Address 1:"
+                fill_in 'organizationAddrees1', :with => value
+            when "City 1:"
+                fill_in 'organizationCity', :with => value
+            when "Country 1:"
+                find('#organizationCountry').find(:xpath, '/html/body/div/div[1]/div/form/div/div[4]/div[6]/div/select/option[28]').select_option
+            when "Zip Code 1:"
+                fill_in 'organizationPostal' , :with => value
+        end
+    end 
+end
+
+Given('I login with data below') do |table|
+    data = table.rows_hash
+    data.each_pair do |key, value|
+        case key
+            when "Email:"
+                fill_in 'email-login', :with => value
+            when "Password:"
+                fill_in 'password-login', :with => value
+        end
+    end
+end
+  
+When('I press Login button') do
+    find(:xpath, '/html/body/div[1]/div/div/div/div[1]/form/button[1]/span').click
+end
+
+Given('I select all days') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+When('I press {string}') do |buttonName|
+    click_link(buttonName)
+end
+  
+Then('Should show {string} page') do |text|
+    page.has_content?(text)
+end
+
+Then('Should show the user {string} in the table') do |email|
+    find('tr', text: email)
+end
+  
+
+Given('I fill the register as below') do |table|
+    data = table.rows_hash
+    data.each_pair do |key, value|
+        case key
+            when "First Name:"
+                fill_in 'firstName1', :with => value
+            when "Last Name:"
+                fill_in 'lastName1', :with => value
+            when "Email:"
+                fill_in 'email1', :with => value
+            when "Password:"
+                fill_in 'passwordRegister1', :with => value
+            when "Confirm Password:"
+                fill_in 'confirmPasswordRegister1', :with => value
+            when "Gender:"
+                find('#gender1').find(:xpath, '/html/body/div/div[1]/main/div/div/div[6]/form/div/div/div/div[3]/div[7]/div/select/option[1]').select_option
+            when "Food Preference:"
+                find('#foodPreference1').find(:xpath, '/html/body/div/div[1]/main/div/div/div[6]/form/div/div/div/div[3]/div[9]/div/select/option[2]').select_option
+            when "Tshirt Size:"
+                select value, :from => "tshirtSize1"
+            when "Zip Code 1:"
+                fill_in 'monday1' , :with => value
+        end
+    end 
+end
+
+Given('I press Next button on user {int}') do |number|
+    find('#basicInfoForm'+number.to_s+'-nextButton').click
+end
+    
+Given('I leave the selected days') do
+    log(find('#monday1').text)
+end
+    
+Given('I press Next button on conference {int}') do |number|
+    find('#conferenceOptionsForm'+number.to_s+'-nextButton').click
+end
+
+Given('I press Finalize Registration button on user {int}') do |number|
+    find('#registerButton'+number.to_s).click
 end
