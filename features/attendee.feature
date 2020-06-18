e85f423ca3f4715375988a992d078bacc621395e
IvySaskia <saskiaivy@outlook.com>
2020-06-18 15:28:17 -0400

    WIP: attendee

--- features/attendee.feature
+++ features/attendee.feature
@@ -16,12 +16,12 @@ Scenario: Fill registration
     |Address:	    	| Av. Calampamapa    |
     |City: 	            | Cochabamba         |
     |Postal/Zipcode:    | 591                |
-    And I select country option as "Bolivia" 
+    And I select "country" option as "Bolivia" 
     And I click the "NEXT" button
     And I enter the required fields for conference as show below
-    And I select "Gender" option "Male"
-    And I select "Tshirt Size" option "L"
-    And I select "Food Preference" option "Normal"
+    And I select "Gender" option as "Male"
+    And I select "Tshirt Size" option as "L"
+    And I select "Food Preference" option as "Normal"
     And I select days from "Monday" to "Friday"
     And I select the option "to go to the  social Event"
     And I select the option "to be added to the Mailling List"
@@ -38,42 +38,3 @@ Scenario: Fill registration
     And I click the "FINALIZE REGISTRATION" button
     Then the Registration Summary is show
 	And my "First Name" name is "Brayan"
-
-Scenario: Get pdf invoce after I filled registration        
-	Given I am on the ESUG homepage
-	And I click the "REGISTER" link
-	When I enter the required fields for user as show below
-    |First Name: 	    | Brayan             |
-    |Last Name: 	    | Sejas              |
-    |Email: 	        | brayan@gmail.com   |
-    |Password:          | 12345678           | 
-    |Confirm Password:  | 12345678           |
-    And I click the "NEXT" button
-    And I enter the required fields for contact as show below
-    |Address:	    	| Av. Calampamapa    |
-    |City: 	            | Cochabamba         |
-    |Postal/Zipcode:    | 591                |
-    And I select country option as "Bolivia" 
-    And I click the "NEXT" button
-    And I enter the required fields for conference as show below
-    And I select "Gender" option "Male"
-    And I select "Tshirt Size" option "L"
-    And I select "Food Preference" option "Normal"
-    And I select days from "Monday" to "Friday"
-    And I select the option "to go to the  social Event"
-    And I select the option "to be added to the Mailling List"
-    And I click the "NEXT" button
-    And I enter the required fields for billing as show below
-    |Name:	    	    | Sejas              |
-    |Address 1: 	    | Av. Calampampa     |
-    |City: 	            | Cochabamba         |
-    |Postal/Zipcode:    | 591                |     
-    And I click the "NEXT" button
-    And I see addition section
-    And I click the "NEXT" button
-    And I see payment section
-    And I click the "FINALIZE REGISTRATION" button
-    Then the Registration Summary is show
-	And my "First Name" name is "Brayan"
-    And I click "GET INVOICE" button
-    And I see my downloaded invoice
--- features/step_definitions/attendee_steps.rb
+++ features/step_definitions/attendee_steps.rb
@@ -1,67 +1,87 @@
 Given('I am on the ESUG homepage') do
-    page.driver.browser.manage.window.maximize
-    visit ('http://semantics.bo:8000/ESUG')
+  page.driver.browser.manage.window.maximize
+  visit ('http://semantics.bo:8000/ESUG')
 end
-  
-  Given('I click the {string} link') do |string|
-    click_link(linkText)
-  end
-  
-  When('I enter the required fields for user as show below') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I click the {string} button') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I enter the required fields for contact as show below') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I select option {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I enter the required fields for conference as show below') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I select {string} option {string}') do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I select checkbox {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I enter the required fields for billing as show below') do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I see addition section') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When('I see payment section') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('the Registration Summary is show') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('my {string} name is {string}') do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('I click {string} button') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Then('I download the invoice') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+
+Given('I click the {string}') do |string|
+  find(:xpath, '/html/body/div[1]/div/div/div/div[1]/form/button[2]').clickend
+
+When('I enter the required fields for user as show below') do |table|
+  data = table.rows_hash
+  data.each_pair do |key, value|
+    case key
+    when "First Name:"
+      fill_in 'firstName', :with => value
+      @name = value
+    when "Last Name:"
+      fill_in 'lastName', :with => value
+      @lastName = value
+    when "Email:"
+      fill_in 'email', :with => value
+      @email = value
+    when "Password:"
+      fill_in 'password', :with => value
+    when "Confirm Password:"
+      fill_in 'confirmPassword', :with => value
+      @password = value
+    when "Address:"
+      fill_in 'address1', :with => value
+    when "City:"
+      fill_in 'city', :with => value
+    when "State/Province:"
+      fill_in 'state', :with => value
+    when "Postal Code:"
+      fill_in 'postalCode', :with => value
+    when "Country:"
+      select(value, :from => 'country')
+    when "User Name:"
+      fill_in 'email', :with => value
+      @userName = value
+    end
+end
+
+When('I click the {string} button') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter the required fields for contact as show below') do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I select {string} option as {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter the required fields for conference as show below') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I select days from {string} to {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I select the option {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter the required fields for billing as show below') do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I see addition section') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I see payment section') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the Registration Summary is show') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('my {string} name is {string}') do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
